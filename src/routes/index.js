394c83f0065e5e852f6172cbb483df196e55a0ca
YogiBabuu <konrad1999@wp.pl>
2023-04-10 07:31:45 +0200

    All hapi endpoints linked to db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,17 @@
+import { addViewToListingRoute } from './addViewToListing';
+import { createNewListingRoute } from './createNewListing';
+import { deleteListingRoute } from './deleteListing';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
+import { getUserListingsRoute } from './getUserListings';
+import { updateListingRoute } from './updateListing';
 
 export default [
+    deleteListingRoute,
+    updateListingRoute,
+    createNewListingRoute,
+    getUserListingsRoute,
+    addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
 ];
